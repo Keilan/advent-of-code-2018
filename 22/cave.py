5d0b5d80601ecae7c2c81b16c103486bbe6adab0
keilan <keilan@scholtek.com>
2018-12-22 22:42:25 -0700

    Solved and cleaned up

--- 22/cave.py
+++ 22/cave.py
@@ -1,5 +1,14 @@
 import tqdm
 import numpy as np
+from sortedcontainers import SortedList
+
+INFINITY = 1000000000  # Used for costs of impossible things
+equipment_to_int = {
+    'torch': 0,
+    'climb': 1,
+    'neither': 2
+}
+int_to_equipment = {v: k for k, v in equipment_to_int.items()}
 
 
 def get_erosion_level(x, y, cave, depth, target):
@@ -19,91 +28,109 @@ def get_erosion_level(x, y, cave, depth, target):
     return el
 
 
-def get_movement_time(type1, type2, equipment, to_target=False):
+def movement_cost_function(type1, type2, equipment):
     """
-    Calculates the movement time switching equipment (if needed) and moving between regions, where
-    0 is rocky, 1 is wet, and 2 is narrow. Note that moving to the target is a special case where
-    the region is rocky but you still need to switch to the torch. The new equipment being used
-    is also returned.
+    The cost of moving from an area of type 1 to type 2 where 0 is rocky, 1 is wet, and 2 is narrow.
     """
-    # If the types are the same, no change is needed
     if type1 == type2:
-        time, equipment = 1, equipment
-    else:
-        # There is only one common item between 2 different regions, so the direction isn't important
-        type1, type2 = sorted([type1, type2])
+        return 1
 
-        if type1 == 0 and type2 == 1:
-            time, equipment = 1 if equipment == 'climb' else 8, 'climb'  # Climbing gear is the common item
-        elif type1 == 0 and type2 == 2:
-            time, equipment = 1 if equipment == 'torch' else 8, 'torch'
-        elif type1 == 1 and type2 == 2:
-            time, equipment = 1 if equipment == 'neither' else 8, 'neither'
-        else:
-            raise ValueError('Invalid type combination')
+    # There is only one common item between 2 different regions, so the direction isn't important
+    type1, type2 = sorted([type1, type2])
 
-    # If we're moving to the target and not using the torch, at 7 minutes to switch
-    if to_target and equipment != 'torch':
-        time += 7
+    if type1 == 0 and type2 == 1 and equipment == 'climb':
+        return 1
+    elif type1 == 0 and type2 == 2 and equipment == 'torch':
+        return 1
+    elif type1 == 1 and type2 == 2 and equipment == 'neither':
+        return 1
 
-    return time, equipment
+    return INFINITY  # Other movements are impossible
+
+
+def tool_cost_function(terrain, tool):
+    if terrain == 0 and tool in ['climb', 'torch']:
+        return 7
+    elif terrain == 1 and tool in ['climb', 'neither']:
+        return 7
+    elif terrain == 2 and tool in ['torch', 'neither']:
+        return 7
+
+    return INFINITY  # Other changes are impossible
 
 
 def find_shortest_time(source, target, cave):
     """
-    Use a modified version of Djikstra's algorithm"""
-
+    Use a modified version of Djikstra's algorithm.
+    """
     # Initialize algorithm
     unvisited = set()
     distances = {}
-    equipment = {}  #The possible equipped items at the shortest path to (x,y)
+    options = SortedList()  # Used for finding minimum - stores in format (distance, x, y, t)
     for x in range(cave.shape[0]):
         for y in range(cave.shape[1]):
-            distances[(x, y)] = float('inf')
-            unvisited.add((x, y))
+            for t in equipment_to_int.values():
+                unvisited.add((x, y, t))
+                distances[(x, y, t)] = float('inf')
     progress_bar = tqdm.tqdm(total=len(unvisited))
 
     # Set source to 0
-    distances[(source[0], source[1])] = 0
-    equipment[(source[0], source[1])] = ['torch']
+    distances[source[0], source[1], equipment_to_int['torch']] = 0
+    options.add((0, source[0], source[1], equipment_to_int['torch']))
 
     # Iterate through set
     while unvisited:
-        # Find unvisited node with minimum ditance
-        options = [(k, v) for k, v in distances.items() if k in unvisited]
-        selected, selected_distance = min(options, key=lambda x: x[1])
-        equipment_options = equipment[selected]
+        # Find unvisited node with minimum distance
+        selected = options[0]
+        selected_distance, sel_x, sel_y, sel_t = selected
 
-        unvisited.remove(selected)
+        unvisited.remove((sel_x, sel_y, sel_t))
+        options.remove(selected)
         progress_bar.update(1)
 
-        # Update all neighbours
-        sel_x, sel_y = selected
-        for current_equipment in equipment_options:
-            for x, y in [(sel_x+1, sel_y), (sel_x, sel_y+1), (sel_x-1, sel_y), (sel_x, sel_y-1)]:
-                if (x, y) in unvisited:
-                    # Compute time to move to this node
-                    to_target = (x, y) == target
-                    time_to_move, new_equipment = get_movement_time(cave[sel_x, sel_y], cave[x, y],
-                                                                    current_equipment, to_target)
-                    new_distance = selected_distance + time_to_move
+        # Update all by movement neighbours
+        for x, y in [(sel_x+1, sel_y), (sel_x, sel_y+1), (sel_x-1, sel_y), (sel_x, sel_y-1)]:
+            if (x, y, sel_t) in unvisited:
+                # Compute time to move to this node
+                time_to_move = movement_cost_function(cave[sel_x, sel_y], cave[x, y],
+                                                       int_to_equipment[sel_t])
+                new_distance = selected_distance + time_to_move
 
-                    # Set updated distance if new distance
-                    if new_distance < distances[(x, y)]:
-                        distances[(x, y)] = new_distance
-                        equipment[(x, y)] = [new_equipment]  # Shorter distance, overwrite
+                # Set updated distance if new distance
+                if new_distance < distances[(x, y, sel_t)]:
+                    # Update options for minimum computation
+                    options.discard((distances[x, y, sel_t], x, y, sel_t))
+                    options.add((new_distance, x, y, sel_t))
 
-                    elif (new_distance == distances[(x, y)] and
-                          new_equipment not in equipment[(x, y)]):
-                        equipment[(x, y)].append(new_equipment)  # Add to list of options
+                    distances[(x, y, sel_t)] = new_distance
+
+        # Update all equipment neighbours
+        for t in range(3):
+
+            # Changing to the current tool makes no sense
+            if t == sel_t:
+                continue
+
+            if (sel_x, sel_y, t) in unvisited:
+                # Compute time to change
+                time_to_change = tool_cost_function(cave[sel_x, sel_y], int_to_equipment[t])
+                new_distance = selected_distance + time_to_change
+
+                if new_distance < distances[(sel_x, sel_y, t)]:
+                    # Update options for minimum computation
+                    options.discard((distances[sel_x, sel_y, t], sel_x, sel_y, t))
+                    options.add((new_distance, sel_x, sel_y, t))
+
+                    distances[(sel_x, sel_y, t)] = new_distance
 
     progress_bar.close()
-    return distances[target]
+    return distances[(target[0], target[1], equipment_to_int['torch'])]
 
 
 def cave(depth, target):
     # Create a numpy array from the cave mouth to the target - buffer of 100 for part 2
-    cave = np.full([target[0] + 1, target[1] + 1], -1)
+    cave = np.full([target[0] + 100, target[1] + 1000], -1)
+    print('Traversing cave with shape {}.'.format(cave.shape))
 
     # Iterate over cave, filling in x and y values
     for x in range(cave.shape[0]):
@@ -134,8 +161,4 @@ def print_cave(cave):
         print(output)
 
 
-cave(510, (10, 10))
 cave(3066, (13, 726))
-
-# Wrong - 1019
-# Wrong - 1007
